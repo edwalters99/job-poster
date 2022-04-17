57676d2325a67d9b1ae05b7d80049801702a39e1
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-04-17 15:23:55 +1000

    user address_suburb field made non-compulsory

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -137,7 +137,7 @@ class JobsController < ApplicationController
     end
 
     def full_address_not_supplied
-      @current_user.address_num.blank? || @current_user.address_street.blank? ||  @current_user.address_suburb.blank? || @current_user.address_city.blank? || @current_user.address_country.blank? ||  @current_user.address_postcode.blank? 
+      @current_user.address_num.blank? || @current_user.address_street.blank? ||  @current_user.address_city.blank? || @current_user.address_country.blank? ||  @current_user.address_postcode.blank? 
   end
 
 end
--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,13 @@
 module JobsHelper
     def full_address_not_supplied
-        @current_user.address_num.blank? || @current_user.address_street.blank? ||  @current_user.address_suburb.blank? || @current_user.address_city.blank? || @current_user.address_country.blank? ||  @current_user.address_postcode.blank? 
+        @current_user.address_num.blank? || @current_user.address_street.blank? ||  @current_user.address_city.blank? || @current_user.address_country.blank? ||  @current_user.address_postcode.blank? 
     end
 
     def full_address user
-        "#{ user.address_num } #{ user.address_street }, #{ user.address_suburb }, #{ user.address_city }. #{ user.address_country }. #{ user.address_postcode }"
+        if @current_user.address_suburb.blank?
+            "#{ user.address_num } #{ user.address_street }, #{ user.address_city }. #{ user.address_country }. #{ user.address_postcode }"
+        else
+            "#{ user.address_num } #{ user.address_street }, #{ user.address_suburb }, #{ user.address_city }. #{ user.address_country }. #{ user.address_postcode }"
+        end
     end
 end
